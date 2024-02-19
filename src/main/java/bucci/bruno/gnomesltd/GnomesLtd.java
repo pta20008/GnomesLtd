5a8e3ab6044d8cceee9750a266a040d3bca9eb90
pta20008 <pta20008@student.cct.ie>
2024-02-19 19:34:47 +0000

    Test
    Test

---
+++ src/main/java/bucci/bruno/gnomesltd/GnomesLtd.java
@@ -0,0 +1,16 @@
+/*
+ * https://github.com/pta20008/GnomesLtd.git
+ */
+
+package bucci.bruno.gnomesltd;
+
+/**
+ *
+ * @author bruno
+ */
+public class GnomesLtd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Let's start...");
+    }
+}
